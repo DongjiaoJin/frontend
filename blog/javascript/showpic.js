a94330c1342c1817d39dcef71f6f8f91710586b7
DongjiaoJin <616306992@qq.com>
2017-06-27 20:00:11 +0800

    show description of image in one page
    p.firstChild.nodeValue
    
    Signed-off-by: DongjiaoJin <616306992@qq.com>

--- blog/javascript/showpic.js
+++ blog/javascript/showpic.js
@@ -4,6 +4,9 @@ function showPic(whichpic){
 	var source = whichpic.getAttribute('href');
 	var placeholder = document.getElementById('placeholder');
 	placeholder.setAttribute("src",source);
+	var text = whichpic.getAttribute('title');
+	var ptitle = document.getElementById('ptitle');
+	ptitle.firstChild.nodeValue = text;
 
 
 }
